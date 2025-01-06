c690bc997879f3d4d02fda7de218968a707bcf95
Enzo Martinez <enzomartinez2412@gmail.com>
2025-01-06 11:51:39 -0500

    CORS fix

--- src/main/java/com/bestforpets/Controller/Usuarios/UsuariosController.java
+++ src/main/java/com/bestforpets/Controller/Usuarios/UsuariosController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/auth")
 public class UsuariosController {
     private final UsuariosService usuariosService;
